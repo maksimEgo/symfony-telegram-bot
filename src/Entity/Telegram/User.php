7995a3e90eb817351884705f6acca5ab551e1aa6
Chantico <131584072+KianticO@users.noreply.github.com>
2024-09-03 07:45:56 +0300

    feat(entity): Add user entity

---
+++ src/Entity/Telegram/User.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Entity\Telegram;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class User
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private readonly int $id;
+
+    #[ORM\Column(name: 'user_name', type: 'string', length: 255)]
+    private string $userName;
+
+    #[ORM\Column(name: 'user_chat_id', type: 'integer', nullable: false)]
+    private string $userChatId;
+
+    #[ORM\Column(name: 'first_name', type: 'string', length: 255)]
+    private string $firstName;
+
+    #[ORM\Column(name: 'last_name', type: 'string', length: 255)]
+    private string $lastName;
+
+    #[ORM\Column(name: 'is_active', type: 'boolean')]
+    private bool $isActive;
+
+    #[ORM\Column(name: 'created_at', type: 'datetime_immutable')]
+    private readonly \DateTimeImmutable $createdAt;
+
+    #[ORM\Column(name: 'updated_at', type: 'datetime_immutable')]
+    private \DateTimeImmutable $updatedAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTimeImmutable();
+        $this->updatedAt = new \DateTimeImmutable();
+        $this->isActive = true;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUserName(): string
+    {
+        return $this->userName;
+    }
+
+    public function setUserName(string $userName): void
+    {
+        $this->userName = $userName;
+    }
+
+    public function getUserChatId(): string
+    {
+        return $this->userChatId;
+    }
+
+    public function setUserChatId(string $userChatId): void
+    {
+        $this->userChatId = $userChatId;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getIsActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): void
+    {
+        $this->isActive = $isActive;
+    }
+
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function getUpdatedAt(): \DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeImmutable $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function update(): void
+    {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
+}
